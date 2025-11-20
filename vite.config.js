ec6cd79acaab5049fd9bc85338154e7646010211
Stacey Kremer <staceybumblebee@gmail.com>
2025-11-21 04:50:44 +0500

    build update

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// Если ты не используешь Vue, просто удали строку с плагином и его импорт
-
 export default defineConfig({
   plugins: [vue()],
-  base: '/', // для локальной разработки
+  base: '/TON-3D-model/', 
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
