dd4fcf8bdd72478874bd2a644721204beefc6c53
Stacey Kremer <staceybumblebee@gmail.com>
2025-11-21 05:04:40 +0500

    final working build

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const isProd = process.env.NODE_ENV === 'production'
+
 export default defineConfig({
   plugins: [vue()],
-  base: '/TON-3D-model/', 
+  base: isProd ? '/TON-3D-model/' : '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
+    copyPublicDir: true
   },
   server: {
     port: 5173,
-    open: true,
-  },
+    open: true
+  }
 })
